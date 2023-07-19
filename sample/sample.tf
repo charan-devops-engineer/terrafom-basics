6e9aacc0b42a420dc3be2d84f78ec9e24c89cf6d
sai <saicharantambali@gmail.com>
2023-07-19 23:56:22 +0530

    auto play

--- sample/sample.tf
+++ sample/sample.tf
@@ -1,4 +1,6 @@
 output "sai" {
   value       =     "welcome to sai's terrafom learning"
   value       =      " hardwork pays"
+  value       =      " dont give up"
+  value = " every thing is possible"
 }
